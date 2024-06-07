6ce2c47db7f26141e16e72fed38e5365d6f6e298
viniciusfinger <fingervinicius@icloud.com>
2024-06-07 06:57:38 -0300

    Changing description to final

--- src/main/java/com/viniciusfinger/rsflood/enums/BiologicalSex.java
+++ src/main/java/com/viniciusfinger/rsflood/enums/BiologicalSex.java
@@ -7,7 +7,7 @@ public enum BiologicalSex {
     MALE("Masculino"),
     FEMALE("Feminino");
 
-    private String description;
+    private final String description;
 
     BiologicalSex(String description) {
         this.description = description;
