4c7676627b3a0a5b816fe5291b50be1199677dec
viniciusfinger <fingervinicius@icloud.com>
2024-07-05 19:23:30 -0300

    Creating address service

--- frontend/src/app/services/address.service.ts
+++ frontend/src/app/services/address.service.ts
@@ -1,16 +1,25 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { Address } from '../model/address';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AddressService {
-  private readonly addressUrl = 'localhost:8080/address'; //todo: temporary
+  private readonly addressUrl = 'localhost:8080/addresses'; //todo: temporary, move to env var
 
   constructor(private http: HttpClient) {}
 
   deleteById(id: Number): Observable<any> {
-    return this.http.get(`${this.addressUrl}/${id}`);
+    return this.http.delete(`${this.addressUrl}/${id}`);
+  }
+
+  findAll(): Observable<Address[]> {
+    return this.http.get<Address[]>(this.addressUrl);
+  }
+
+  create(address: Address): Observable<Address> {
+    return this.http.post<Address>(this.addressUrl, address);
   }
 }
